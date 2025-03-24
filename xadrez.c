d6f4d35f4010374de9f00d306dd6d5599f0a6289
emanuelfelipe30 <emanuelfelipe30@gmail.com>
2025-03-24 22:41:30 +0000

    commit final - nível aventureiro

--- xadrez.c
+++ xadrez.c
@@ -27,5 +27,22 @@ int main() {
         k++;
     } while (k < 8);
     
+    printf("\n");
+    
+    // Movimento do Cavalo (2 casas para baixo e 1 para a esquerda) usando loops aninhados
+    printf("Movimento do Cavalo:\n");
+    
+    // Primeiro loop for para mover duas casas para baixo
+    for (int m = 0; m < 2; m++) {
+        printf("Baixo\n");
+    }
+    
+    // Segundo loop while para mover uma casa para a esquerda
+    int n = 0;
+    while (n < 1) {
+        printf("Esquerda\n");
+        n++;
+    }
+    
     return 0;
 }
